e1ddb15abaf042ac86984fb926a0299fc42e8feb
VQSHR <stoneliuicloud@icloud.com>
2023-07-08 14:01:44 -0400

    fish can move based on input

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
+
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(move * Time.deltaTime);
     }
 }
