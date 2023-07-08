24734dc88997feb89cb32618e4d2e9bf19e8b1de
OtakuZhang <zhangyk@shanghaitech.edu.cn>
2023-07-08 18:13:39 -0400

    updated spawn at random point

--- Assets/Scripts/Beatmap.cs
+++ Assets/Scripts/Beatmap.cs
@@ -9,17 +9,21 @@ public class Beatmap : MonoBehaviour
     public GameObject KnifeSpawn1;
     public GameObject KnifeSpawn2;
     public GameObject KnifeParent;
+    private GameObject[] SpawnPoints;
 
     public void SpawnNote()
     {
-        GameObject Note = Instantiate(KnifeChild, KnifeSpawn1.transform.position, KnifeSpawn1.transform.rotation, KnifeParent.transform);
+        bool if_note = (Random.Range(0, 10) > 4);
+        int randomIndex = Random.Range(0, 3);
+        if (if_note)
+            Instantiate(KnifeChild, SpawnPoints[randomIndex].transform.position, SpawnPoints[randomIndex].transform.rotation, KnifeParent.transform);
     }
     
     
     // Start is called before the first frame update
     void Start()
     {
-        //SpawnNote();
+        SpawnPoints = new GameObject[] {KnifeSpawn0, KnifeSpawn1, KnifeSpawn2};
     }
 
     // Update is called once per frame
--- Assets/Scripts/KnifeNote.cs
+++ Assets/Scripts/KnifeNote.cs
@@ -8,24 +8,47 @@ public class KnifeNote : MonoBehaviour
     private float endy = 23.8f;
     private float initialWaitSecond = 0.4f;
     private float endWaitSecond = 0.2f;
+
+    public AudioClip yellClip;
+    public AudioClip dropClip;
+    public AudioClip hitClip;
+    private AudioSource audioSource;
     
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         StartCoroutine(NoteBehavior());
     }
 
     private IEnumerator NoteBehavior()
     {
+        audioSource.clip = yellClip;
+        audioSource.Play();
         yield return new WaitForSeconds(initialWaitSecond);
 
         // Start dropping
-        while (transform.position.y > endy)
+        audioSource.clip = dropClip;
+        audioSource.Play();
+        if (transform.position.y > endy)
         {
-            transform.position -= new Vector3(0, dropSpeed * Time.deltaTime, 0);
-            yield return null;
-        }
+            while (transform.position.y > endy)
+            {
+                transform.position -= new Vector3(0, dropSpeed * Time.deltaTime, 0);
+                yield return null;
+            }
 
+            audioSource.clip = hitClip;
+            audioSource.Play();
+        }
+        else
+        {
+            while (transform.position.z < 14.4f)
+            {
+                transform.position += new Vector3(0, 0, 2f * dropSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
         yield return new WaitForSeconds(endWaitSecond);
 
         // Make the note disappear
